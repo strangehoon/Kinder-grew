508723a83f9e08fe1242bfc3f0b748e9b40cba74
Sanghoon Lee <strangehoon2@gmail.com>
2023-04-06 15:42:43 +0900

    Refactor : 더미데이터 오픈

--- src/main/java/com/sparta/finalproject/global/procedure/MyStoredProcService.java
+++ src/main/java/com/sparta/finalproject/global/procedure/MyStoredProcService.java
@@ -1,15 +1,15 @@
-//package com.sparta.finalproject.global.procedure;
-//
-//import lombok.RequiredArgsConstructor;
-//import org.springframework.jdbc.core.JdbcTemplate;
-//import org.springframework.stereotype.Service;
-//
-//@Service
-//@RequiredArgsConstructor
-//public class MyStoredProcService {
-//    private final JdbcTemplate jdbcTemplate;
-//
-//    public void callMyStoredProc() {
-//        jdbcTemplate.execute("CALL attendance_add_procedure()");
-//    }
-//}
+package com.sparta.finalproject.global.procedure;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class MyStoredProcService {
+    private final JdbcTemplate jdbcTemplate;
+
+    public void callMyStoredProc() {
+        jdbcTemplate.execute("CALL attendance_add_procedure()");
+    }
+}
