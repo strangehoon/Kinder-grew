e7bc88bdc9f2d10d04ffa1c4d038010261ba86dc
yshong1998 <yshong1998@gmail.com>
2023-04-10 19:35:23 +0900

    Style: CustomStatusCode의 성공 실패 코드를 하나의 단락에 위치시키도록 변경
    #Resolves:
    #Ref:

--- src/main/java/com/sparta/finalproject/global/response/CustomStatusCode.java
+++ src/main/java/com/sparta/finalproject/global/response/CustomStatusCode.java
@@ -9,40 +9,50 @@ import lombok.Getter;
 public enum CustomStatusCode {
 
     //Global
-    NOT_VALID_REQUEST(400,  "유효하지 않은 요청입니다."),
-    NOT_VALID_TOKEN(400,"유효한 토큰이 아닙니다."),
-    TOKEN_NOT_FOUND(400,  "토큰이 없습니다."),
-
-    //Success
+    NOT_VALID_REQUEST(400, "유효하지 않은 요청입니다."),
+    NOT_VALID_TOKEN(400, "유효한 토큰이 아닙니다."),
+    TOKEN_NOT_FOUND(400, "토큰이 없습니다."),
+    UNAUTHORIZED_USER(400, "인가되지 않은 사용자입니다."),
 
     //User 관련
     SIGN_UP_SUCCESS(200, "회원 가입에 성공했습니다."),
     LOG_IN_SUCCESS(200, "로그인되었습니다."),
-    ESSENTIAI_INFO_EMPTY(200, "추가적인 정보 입력이 필요합니다."),
-    ESSENTIAI_INFO_EXIST(201, "추가적인 정보가 이미 존재합니다."),
-    FINAL_SIGNUP_PARENT(200, "회원가입을 축하드립니다."),
-    FINAL_SIGNUP_TEACHER(200, "회원가입을 축하드립니다."),
-    FINAL_SIGNUP_PRINCIPAL(200, "회원가입을 축하드립니다."),
+    ESSENTIAL_INFO_EMPTY(200, "추가적인 정보 입력이 필요합니다."),
+    ESSENTIAL_INFO_EXIST(201, "추가적인 정보가 이미 존재합니다."),
+    FINAL_SIGNUP_SUCCESS(200, "회원가입을 축하드립니다."),
     INSERT_PRINCIPAL_INFO_SUCCESS(200, "원장 선생님 추가 정보가 입력되었습니다."),
-    DIFFRENT_ADMIN_TOKEN(400,"관리자 비밀번호가 틀렸습니다."),
-    USER_NOT_FOUND(400,"사용자를 찾을 수 없습니다."),
     PROFILE_INFO_GET_SUCCESS(200, "유저 프로필 정보 조회 성공."),
-    DIFFRENT_ROLE(400, "권한이 없습니다."),
+    FIND_TEACHER_LIST_SUCCESS(200, "선생님 목록이 조회되었습니다."),
     PROFILE_INFO_CHANGE_SUCCESS(200, "프로필 정보 수정 완료."),
+    MODIFY_CLASSROOM_TEACHER_SUCCESS(200, "담임 선생님이 설정되었습니다."),
+
+    USER_NOT_FOUND(400, "사용자를 찾을 수 없습니다."),
+    DIFFERENT_ROLE(400, "권한이 없습니다."),
+    PARENT_NOT_FOUND(400, "학부모를 찾을 수 없습니다."),
+    SET_TEACHER_INFO_FAIL(400, "선생님 정보가 설정되지 않았습니다."),
+    TEACHER_NOT_FOUND(400, "선생님을 찾을 수 없습니다."),
+
+
 
 
     //Classroom 관련
     ADD_CLASSROOM_SUCCESS(200, "반이 생성되었습니다."),
-    FIND_CLASSROOM_SUCCESS(200,"반이 로드되었습니다."),
-    MODIFY_TEACHER_SUCCESS(200,"담임 선생님 정보가 설정되었습니다."),
+    FIND_CLASSROOM_SUCCESS(200, "반이 로드되었습니다."),
+    MODIFY_TEACHER_SUCCESS(200, "담임 선생님 정보가 설정되었습니다."),
+
+    CLASSROOM_NOT_FOUND(400, "반을 찾을 수 없습니다."),
 
     //ImagePost 관련
-    ADD_IMAGE_POST_SUCCESS(200,"사진 게시글이 등록되었습니다."),
-    FIND_IMAGE_LIST_SUCCESS(200,"사진 목록이 로드되었습니다."),
+    ADD_IMAGE_POST_SUCCESS(200, "사진 게시글이 등록되었습니다."),
+    FIND_IMAGE_LIST_SUCCESS(200, "사진 목록이 로드되었습니다."),
     FIND_IMAGE_POST_PAGE_SUCCESS(200, "사진 게시글 목록이 로드되었습니다."),
     MODIFY_IMAGE_POST_SUCCESS(200, "사진 게시글이 수정되었습니다."),
     DELETE_IMAGE_POST_SUCCESS(200, "사진 게시글이 삭제되었습니다."),
 
+    IMAGE_POST_NOT_FOUND(400, "사진 게시글을 찾을 수 없습니다."),
+    IMAGE_UPLOAD_FAIL(400, "이미지 업로드에 실패했습니다."),
+    FilE_CONVERT_FAIL(400, "파일 전환에 실패했습니다."),
+    FILE_DELETE_FAIL(400, "파일 삭제에 실패했습니다."),
 
     // child 관련
     ADD_CHILD_SUCCESS(200, "아이 정보가 생성되었습니다."),
@@ -50,26 +60,24 @@ public enum CustomStatusCode {
     FIND_CHILD_SUCCESS(200, "아이 정보가 로드되었습니다."),
     MODIFY_CHILD_SUCCESS(200, "아이 정보가 수정되었습니다."),
     SEARCH_CHILD_SUCCESS(200, "검색한 아이 정보가 로드되었습니다."),
-
     GET_CHILD_PROFILE_SUCCESS(200, "아이 정보가 로드되었습니다."),
-
     UPDATE_CHILD_ATTENDANCE_TIME_SUCCESS(200, "아이의 등하원 시간이 변경 되었습니다."),
+    CHILD_NOT_FOUND(400, "아이를 찾을 수 없습니다."),
 
     // parent 관련
     GET_PARENT_SUCCESS(200, "학부모 페이지가 로드되었습니다."),
     UPDATE_PARENT_SUCCESS(200, "학부모 프로필이 수정되었습니다."),
-    SEARCH_PARENT_SUCCESS(200,"검색한 학부모 목록이 로드되었습니다."),
+    SEARCH_PARENT_SUCCESS(200, "검색한 학부모 목록이 로드되었습니다."),
 
     // attendance 관련
     CHILD_ENTER_SUCCESS(200, "등원 처리가 완료되었습니다."),
-    CHILD_ENTER_CANCEL(200,"등원 처리가 취소되었습니다."),
+    CHILD_ENTER_CANCEL(200, "등원 처리가 취소되었습니다."),
     CHILD_EXIT_SUCCESS(200, "하원 처리가 완료되었습니다."),
     CHILD_EXIT_CANCEL(200, "하원 처리가 취소되었습니다."),
     CHILD_ABSENT_SUCCESS(200, "결석 처리가 완료되었습니다."),
     CHILD_ABSENT_CANCEL(200, "결석 처리가 취소되었습니다."),
-
+    NOT_FOUND_ATTENDANCE(400, "출결 정보를 찾을 수 없습니다."),
     MONTH_ATTENDANCE_LIST_SUCCESS(200, "해당 반의 월별 출결 내역이 조회되었습니다."),
-
     DATE_ATTENDANCE_LIST_SUCCESS(200, "반 별 해당 날짜의 출결 내역이 조회되었습니다."),
 
     // absent 관련
@@ -81,25 +89,7 @@ public enum CustomStatusCode {
     FIND_SCHEDULE_SUCCESS(200, "등하원 기록 조회에 성공했습니다."),
 
     // kindergarten 관련
-
-    KINDERGARTEN_NOT_FOUND(400, "유치원을 찾을 수 없습니다."),
-
-    // exception
-    CLASSROOM_NOT_FOUND(400, "반을 찾을 수 없습니다."),
-    IMAGE_POST_NOT_FOUND(400, "사진 게시글을 찾을 수 없습니다."),
-    CHILD_NOT_FOUND(400, "아이를 찾을 수 없습니다."),
-    SET_TEACHER_INFO_FAIL(400, "선생님 정보가 설정되지 않았습니다."),
-    FilE_CONVERT_FAIL(400, "파일 전환에 실패했습니다."),
-    FILE_DELETE_FAIL(400, "파일 삭제에 실패했습니다."),
-    PARENT_NOT_FOUND(400, "학부모를 찾을 수 없습니다."),
-    IMAGE_UPLOAD_FAIL(400, "이미지 업로드에 실패했습니다."),
-    NOT_FOUND_ATTENDANCE(400, "출결 정보를 찾을 수 없습니다."),
-    UNAUTHORIZED_USER(400, "인가되지 않은 사용자입니다."),
-    TEACHER_NOT_FOUND(400, "선생님을 찾을 수 없습니다."),
-    MODIFY_CLASSROOM_TEACHER_SUCCESS(200, "담임 선생님이 설정되었습니다."),
-    FIND_TEACHER_LIST_SUCCESS(200, "선생님 목록이 조회되었습니다.");
-
-
+    KINDERGARTEN_NOT_FOUND(400, "유치원을 찾을 수 없습니다.");
 
     private final int statusCode;
     private final String message;
