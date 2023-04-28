25f5c1d7779879e47a2b327a2ede4602e7b6b58b
Sanghoon Lee <strangehoon2@gmail.com>
2023-04-28 21:59:10 +0900

    fix : 유치원 생성시 발생하는 조회 오류 해결 #67

--- src/main/java/com/sparta/finalproject/domain/child/repository/ChildRepositoryImpl.java
+++ src/main/java/com/sparta/finalproject/domain/child/repository/ChildRepositoryImpl.java
@@ -14,6 +14,8 @@ import java.util.List;
 
 import static com.sparta.finalproject.domain.attendance.entity.QAttendance.attendance;
 import static com.sparta.finalproject.domain.child.entity.QChild.child;
+import static com.sparta.finalproject.domain.classroom.entity.QClassroom.classroom;
+import static com.sparta.finalproject.domain.kindergarten.entity.QKindergarten.kindergarten;
 import static com.sparta.finalproject.global.enumType.AttendanceStatus.결석;
 import static com.sparta.finalproject.global.enumType.CommuteStatus.ENTER;
 import static com.sparta.finalproject.global.enumType.CommuteStatus.EXIT;
@@ -26,8 +28,8 @@ public class ChildRepositoryImpl implements ChildRepositoryCustom{
     private final JPAQueryFactory queryFactory;
 
     @Override
-    public Page<ChildScheduleResponseDto> findChildSchedule(Long classroomId, CommuteStatus commuteStatus, String time, Pageable pageable,
-                                                            InfoDto info, List<ClassroomInfoDto> everyClass){
+    public Page<ChildScheduleResponseDto> findChildSchedule(Long classroomId, Long kindergartenId, CommuteStatus commuteStatus, String time, Pageable pageable,
+                                                                              InfoDto info, List<ClassroomInfoDto> everyClass){
         QueryResults<ChildScheduleResponseDto> result = queryFactory
                 .select(new QChildScheduleResponseDto(
                         child.id,
@@ -37,13 +39,16 @@ public class ChildRepositoryImpl implements ChildRepositoryCustom{
                         attendance.exitTime,
                         attendance.status
                 ))
-                .from(attendance)
-                .leftJoin(attendance.child, child)
-                .where(classIdIs(classroomId), stateIs(commuteStatus),
+                .from(attendance, child, classroom)
+                .join(attendance.child, child)
+                .join(child.classroom, classroom)
+                .join(classroom.kindergarten, kindergarten)
+                .where(classroomIdAndKindergartenIdIs(classroomId, kindergartenId), stateIs(commuteStatus),
                         timeIs(commuteStatus, time), attendance.date.eq(LocalDate.now()))
                 .orderBy(child.name.asc())
                 .offset(pageable.getOffset())
                 .limit(pageable.getPageSize())
+                .distinct()
                 .fetchResults();
 
         Long total = queryFactory
@@ -55,11 +60,14 @@ public class ChildRepositoryImpl implements ChildRepositoryCustom{
                         attendance.exitTime,
                         attendance.status
                 ))
-                .from(attendance)
-                .leftJoin(attendance.child, child)
-                .where(classIdIs(classroomId), stateIs(commuteStatus),
+                .from(attendance, child, classroom)
+                .join(attendance.child, child)
+                .join(child.classroom, classroom)
+                .join(classroom.kindergarten, kindergarten)
+                .where(classroomIdAndKindergartenIdIs(classroomId, kindergartenId), stateIs(commuteStatus),
                         timeIs(commuteStatus, time), attendance.date.eq(LocalDate.now()))
                 .orderBy(child.name.asc())
+                .distinct()
                 .fetchCount();
         List<ChildScheduleResponseDto> contents = result.getResults();
 
@@ -67,9 +75,10 @@ public class ChildRepositoryImpl implements ChildRepositoryCustom{
         return new CustomPageImpl<>(contents, pageable, total, info, everyClass);
     }
 
-    private BooleanExpression classIdIs(Long classroomId) {
-        return classroomId != null ? child.classroom.id.eq(classroomId) : null;
+    private BooleanExpression classroomIdAndKindergartenIdIs(Long classroomId, Long kindergartenId) {
+        return classroomId != null ? child.classroom.id.eq(classroomId).and(classroom.kindergarten.id.eq(kindergartenId)) : classroom.kindergarten.id.eq(kindergartenId);
     }
+
     private BooleanExpression stateIs(CommuteStatus commuteStatus){
         if(commuteStatus.equals(ENTER)){
             return attendance.exitTime.isNull().and(attendance.date.eq(LocalDate.now())).and(attendance.status.ne(결석));
